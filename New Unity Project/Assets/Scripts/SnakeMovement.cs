2f5c63a97576881d2f621bc2b16132aa6ddd6b3d
Jose Escribano vega <msi_w@hotmail.com>
2018-07-16 23:42:32 +0200

    Movimiento en un plano

---
+++ New Unity Project/Assets/Scripts/SnakeMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovement : MonoBehaviour {
+
+   //velocidad de movimiento del snake
+    public float speed =1f;
+	
+    //Define el movimiento del player en base a las teclas que se pulsen
+    Vector3 movement; 
+	Rigidbody snakeRigibody;
+
+	void Start () 
+	{
+		snakeRigibody= GetComponent<Rigidbody>();
+
+	}
+	
+	void FixedUpdate()
+	{
+		//Inicializamos variables 
+		float moveHorizontal= Input.GetAxis("Horizontal");
+		float moveVertical= Input.GetAxis("Vertical");
+
+		//Llamamos a la función Move 
+		Move(moveHorizontal,moveVertical);
+	}
+
+	//Genera el movimiento del personaje con los inputs del teclado...
+	void Move(float h, float v)
+	{
+		movement.Set(h,0f,v);
+		movement= movement.normalized * speed * Time.deltaTime;
+		snakeRigibody.MovePosition(transform.position+movement);//colisión con objetos de la escena
+
+	}
+
+}
