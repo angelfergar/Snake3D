4a2c5ec5e99ec398a7f0f740bb90e920c9724b60
Angel Fernandez <anfernagar@gmail.com>
2018-07-19 21:42:24 +0200

    Pause/Resume

--- New Unity Project/Assets/Scripts/GameController/GameController.cs
+++ New Unity Project/Assets/Scripts/GameController/GameController.cs
@@ -12,18 +12,26 @@ public class GameController : MonoBehaviour {
     public int count; //Lo que vale cada PickUp
     public Text countText;
     public Button pauseButton;
+    public Text pauseText;
+    public Button resumeButton;
 
 	// Use this for initialization
 	void Start () {
 
         playText.text = "PLAY";
-        //Al pular el botón, empieza el juego
+        pauseText.text = "";
+        //Al pulsar el botón, empieza el juego
         Button btn1 = playButton.GetComponent<Button>();
         Button btn2 = pauseButton.GetComponent<Button>();
+        Button btn3 = resumeButton.GetComponent<Button>();
         btn1.onClick.AddListener(StartGame);
         btn2.onClick.AddListener(PauseGame);
+        btn3.onClick.AddListener(ResumeGame);
+        resumeButton.gameObject.SetActive(false);
+        pauseButton.gameObject.SetActive(false);
         count = 0;
         SetCountText();
+      
         
             
 
@@ -38,12 +46,26 @@ public class GameController : MonoBehaviour {
     {
         empezado = true;
         playText.text = "";
+        pauseButton.gameObject.SetActive(true);
         
+        
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1.0f;
+        pauseText.text = "";
+        pauseButton.gameObject.SetActive(true);
+        resumeButton.gameObject.SetActive(false);
     }
 
     void PauseGame()
     {
-        Time.timeScale = Mathf.Approximately(Time.timeScale, 0.0f) ? 1.0f : 0.0f;
+        Time.timeScale = 0.0f;
+        pauseText.text = "PAUSE";
+        resumeButton.gameObject.SetActive(true);
+        pauseButton.gameObject.SetActive(false);
+
     }
 
     public void SetCountText() //Para llevar la cuenta 
