122c860f8eee656a78998f0f8f54cb8034e6b72a
Angel Fernandez <anfernagar@gmail.com>
2018-07-20 20:36:45 +0200

    Cámara sigue a la cabeza (No rotation)

---
+++ New Unity Project/Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public GameObject reference;
+    private Vector3 offset;
+    
+
+	// Update is called once per frame
+	void Start () {
+
+        offset = transform.position - reference.transform.position;
+	}
+
+    private void LateUpdate() //Se ejecuta cada frame, pero una vez se han terminado todas las acciones de los Update
+    {
+        transform.position = reference.transform.position + offset; 
+    }
+}
