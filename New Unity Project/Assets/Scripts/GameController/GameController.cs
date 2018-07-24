1dbe794e3ff6be3f416fdc3ded483c6d2b11da78
Angel Fernandez <anfernagar@gmail.com>
2018-07-24 20:35:08 +0200

    Show Score + Fixes

--- New Unity Project/Assets/Scripts/GameController/GameController.cs
+++ New Unity Project/Assets/Scripts/GameController/GameController.cs
@@ -16,9 +16,13 @@ public class GameController : MonoBehaviour {
     public Text pauseText;
     public Button resumeButton;
     public Button restartButton;
+    public Text restartText;
+    public Text scoreText;
+    public Text finalCountText;
+    public Image cubeScore;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
         //Botones
         Button btn1 = playButton.GetComponent<Button>();
@@ -31,8 +35,14 @@ public class GameController : MonoBehaviour {
         btn3.onClick.AddListener(ResumeGame);
         btn4.onClick.AddListener(RestartGame);
         //Estado inicial
+        cubeScore.gameObject.SetActive(false);
         playText.text = "PLAY";
         pauseText.text = "";
+        scoreText.text = "";
+        finalCountText.text = "";
+        restartText.text = "";
+        countText.text = "";
+        playButton.gameObject.SetActive(true);
         restartButton.gameObject.SetActive(false);
         resumeButton.gameObject.SetActive(false);
         pauseButton.gameObject.SetActive(false);
@@ -52,16 +62,21 @@ public class GameController : MonoBehaviour {
     void StartGame()
     {
         empezado = true;
+        Time.timeScale = 1.0f;
         playText.text = "";
+        playButton.gameObject.SetActive(false);
+        countText.gameObject.SetActive(true);
         pauseButton.gameObject.SetActive(true);
-        
-        
+        cubeScore.gameObject.SetActive(true);
+
+
     }
 
     void ResumeGame()
     {
         Time.timeScale = 1.0f;
         pauseText.text = "";
+        playButton.gameObject.SetActive(false);
         pauseButton.gameObject.SetActive(true);
         resumeButton.gameObject.SetActive(false);
     }
@@ -70,6 +85,7 @@ public class GameController : MonoBehaviour {
     {
         Time.timeScale = 0.0f;
         pauseText.text = "PAUSE";
+        playButton.gameObject.SetActive(false);
         resumeButton.gameObject.SetActive(true);
         pauseButton.gameObject.SetActive(false);
 
@@ -82,10 +98,17 @@ public class GameController : MonoBehaviour {
 
     public void Dead()
     {
+        restartText.text = "RESTART";
+        scoreText.text = "YOUR SCORE";
+        finalCountText.text = count.ToString();
+        countText.text = "";
+        playButton.gameObject.SetActive(false);
+        cubeScore.gameObject.SetActive(false);
         pauseButton.gameObject.SetActive(false);
         resumeButton.gameObject.SetActive(false);
         restartButton.gameObject.SetActive(true);
-        empezado = false;
+        playButton.gameObject.SetActive(false);
+        Time.timeScale = 0.0f;
     }
 
     void RestartGame()
