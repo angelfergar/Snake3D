e601d87d517f545906afa251e13840f2d518af41
Angel Fernandez <anfernagar@gmail.com>
2018-07-29 16:53:11 +0200

    Revert "Camera Follow Up Deleted"
    This reverts commit 353a008a54256e649ddeed68912ab21f058d7965.

--- New Unity Project/Assets/Scripts/Camera/CameraMovement.cs
+++ New Unity Project/Assets/Scripts/Camera/CameraMovement.cs
@@ -6,19 +6,16 @@ public class CameraMovement : MonoBehaviour {
 
     public GameObject reference;
     private Vector3 offset;
-    public Vector3 rotation;
     
 
 	// Update is called once per frame
 	void Start () {
 
         offset = transform.position - reference.transform.position;
-        rotation = transform.rotation.eulerAngles;
 	}
 
     private void LateUpdate() //Se ejecuta cada frame, pero una vez se han terminado todas las acciones de los Update
     {
-        transform.position = reference.transform.position + offset;
-        transform.rotation = Quaternion.Euler(rotation);
+        transform.position = reference.transform.position + offset; 
     }
 }
